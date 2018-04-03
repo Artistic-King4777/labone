cba7d0e32791288697c88aaffd116a5e0e8c7e60
NightShadow3000 <RoyalFotoK@gmail.com>
2018-04-03 16:54:03 -0400

    updated

--- src/main/java/lab/Main.java
+++ src/main/java/lab/Main.java
@@ -3,5 +3,6 @@ package lab;
 public class Main {
     public static void main(String[] args) {
         System.out.println("IT WORKS BOOYAKASHA!");
+        System.out.println("For the SUPER Negros");
     }
 }
